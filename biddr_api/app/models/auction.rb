039d4d3aa51998b9583fee4121e7401e7c6d0605
betsy_manuel <betsy.manuel03@gmail.com>
2021-03-01 11:14:32 -0800

    created seed file

--- biddr_api/app/models/auction.rb
+++ biddr_api/app/models/auction.rb
@@ -2,8 +2,10 @@ class Auction < ApplicationRecord
 
     has_many :bids, dependent: :destroy
 
-    validates :title, presence: true,uniqueness: true
-    validates :description, presence: true
-    validates :ends_at, presence: true
-    validates :reserve_price, presence: true
+    belongs_to :user, optional: true
+
+    # validates :title, presence: true,uniqueness: true
+    # validates :description, presence: true
+    # validates :ends_at, presence: true
+    # validates :reserve_price, presence: true
 end
--- biddr_api/db/seeds.rb
+++ biddr_api/db/seeds.rb
@@ -6,5 +6,59 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.delete_all
+Auction.delete_all
+Bid.delete_all
 
-{"title":"3 Year Old Mackbook Pro","description":"Good condition","ends_at":"2020-02-02","reserve_price":30}
+NUM_AUCTIONS = 15
+NUM_USER = 10
+
+PASSWORD='supersecret'
+super_user= User.create(
+    first_name: 'Jon',
+    last_name:'Snow',
+    email: 'js@winterfell.gov',
+    password: PASSWORD,
+)
+
+NUM_USER.times do
+    first_name= Faker::Name.first_name 
+    last_name= Faker::Name.last_name 
+    User.create(
+        first_name: first_name,
+        last_name: last_name,
+        email: "#{first_name}.#{last_name}@example.com",
+        password: PASSWORD
+    )
+end
+users=User.all
+
+NUM_AUCTIONS.times do 
+    created_at=Faker::Date.backward(days: 365*5)
+    a = Auction.create(
+        title: Faker::Hacker.say_something_smart,
+        description: Faker::ChuckNorris.fact,
+        ends_at: Faker::Date.forward(days: 100),
+        reserve_price: rand(20..500),
+        created_at: created_at,
+        updated_at: created_at,
+        user: users.sample
+    )
+
+    if a.valid?
+        a.bids = rand(2..6).times.map do
+            Bid.new(
+                price: rand(20..550),
+                user: users.sample
+            )
+        end
+    end
+end
+
+auction = Auction.all
+bid = Bid.all
+
+puts Cowsay.say("Generated #{auction.count} auctions.",:frogs)
+puts Cowsay.say("Generated #{bid.count} bid.",:tux)
+puts Cowsay.say("Generated #{users.count} users.",:beavis)
+puts Cowsay.say("Login with  #{super_user.email} and password:#{PASSWORD}.",:koala)
