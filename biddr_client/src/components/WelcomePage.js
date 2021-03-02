3cd629c70bd9aec2d42de40e2e6a7f589355d6e2
betsy_manuel <betsy.manuel03@gmail.com>
2021-03-01 20:44:29 -0800

    completed final_exam

---
+++ biddr_client/src/components/WelcomePage.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+export class WelcomePage extends Component {
+
+    render() {
+        return (
+            <div class="welcome">
+            </div>
+        )
+    }
+}
+
+export default WelcomePage
